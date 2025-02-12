43a1d2b85f53067f5ed8dc71d8f482c844f4a278
Giriselvan <giriselvansridhar2024@google.com>
2025-02-12 19:11:40 +0530

    Added the Database

--- Authication/models.py
+++ Authication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    number = models.CharField(max_length=10)
+    password = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.number
+    
+
+    
